7344dbd787f505056b0504f001a6a5700b0ff267
Veijleign <hunter_of_the_world@mail.ru>
2023-08-16 19:01:41 +0300

    Added r2dbc Query

--- src/main/java/ru/microservices/action_history_service/domain/repository/HistoryActionRepository.java
+++ src/main/java/ru/microservices/action_history_service/domain/repository/HistoryActionRepository.java
@@ -1,7 +1,29 @@
 package ru.microservices.action_history_service.domain.repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.microservices.action_history_service.domain.entity.HistoryAction;
 
-public interface HistoryActionRepository extends ReactiveCrudRepository<HistoryAction, Long> {
+@Repository
+public interface HistoryActionRepository
+        extends ReactiveCrudRepository<HistoryAction, Long> {
+
+    @Query("""
+            SELECT service_instance_id item
+            FROM history_action
+            WHERE item.service_instance_id =:serviceInstanceId
+            """
+    )
+    Mono<HistoryAction> findTopByServiceInstanceId(Long serviceInstanceId);
+
+    @Query("""
+            SELECT *
+            FROM history_action
+            WHERE item.service_instance_id =:serviceInstanceId
+            """
+    )
+    Flux<HistoryAction> findAllByServiceInstanceId(Long serviceInstanceId);
 }
